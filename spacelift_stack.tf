7a8b506857b749674b7feb01986d7a2eca5b4765
eminalemdar <emnalmdr@gmail.com>
2023-05-11 19:09:54 +0300

    updated stack space

--- spacelift_stack.tf
+++ spacelift_stack.tf
@@ -3,9 +3,9 @@ resource "spacelift_stack" "aws_s3_bucket" {
   description       = "First S3 Bucket Created with Spacelift!"
   repository        = "spacelift-example-stack"
   branch            = "master"
-  project_root      = "bucket/"
+  project_root      = "bucket"
   autodeploy        = true
   terraform_version = "1.4.6"
-  space_id          = "root"
+  space_id          = "legacy"
   worker_pool_id    = var.worker_pool_id
 }
